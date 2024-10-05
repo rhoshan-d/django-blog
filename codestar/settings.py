675bf753485aec7009ea9fe324c5f86648bb4602
Rhoshan <rhoshan02@gmail.com>
2024-10-05 20:40:47 +0100

    Oops fix internal server error caused

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-@&h(!@zndewqu6&m^0$re(ghz(4!5n0_m*(wr!ct8ovp45+fh@'
-SECRET_KEY = os.environ.get("django-insecure-@&h(!@zndewqu6&m^0$re(ghz(4!5n0_m*(wr!ct8ovp45+fh@")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
