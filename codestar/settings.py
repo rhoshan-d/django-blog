a533bd89326e40b723da4fa3877ac4804988c8bf
Rhoshan <rhoshan02@gmail.com>
2024-10-05 20:50:05 +0100

    Another try to fix heroku error 500

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
-    "https://codestar-rd-94512bc7c58e.herokuapp.com"
+    "https://codestar-rd-94512bc7c58e.herokuapp.com/"
 ]
 
 # Password validation
