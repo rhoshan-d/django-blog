014f64718a5e5fbfa5beb9d06262ed50ae565a30
Rhoshan <rhoshan02@gmail.com>
2024-10-06 21:00:05 +0100

    Post model update. Ticket model added

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -14,3 +14,22 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Ticket(models.Model):
+    ticket_holder = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="users_tickets"
+    )
+    date_issued = models.DateTimeField(auto_now_add=True)
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="event_tickets"
+    )
+
+    def __str__(self):
+        return f"Ticket for {self.ticket_holder}"
