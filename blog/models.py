7303af03afc5d034b2e6240f16ed40e8f913e035
Rhoshan <rhoshan02@gmail.com>
2024-10-10 01:36:39 +0100

    feat(post view)

--- blog/models.py
+++ blog/models.py
@@ -17,19 +17,39 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
-    )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return f"Ticket for {self.ticket_holder}"
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+
+# class Ticket(models.Model):
+#     ticket_holder = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE,
+#         related_name="users_tickets"
+#     )
+#     date_issued = models.DateTimeField(auto_now_add=True)
+#     event = models.ForeignKey(
+#         Event,
+#         on_delete=models.CASCADE,
+#         related_name="event_tickets"
+#     )
+
+#     def __str__(self):
+#         return f"Ticket for {self.ticket_holder}"
--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as index_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', index_views.index, name='index'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
