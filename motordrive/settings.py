8d6ab59e4d25bb3e54c62d4be9eeaeb4923843df
Rhoshan <rhoshan02@gmail.com>
2025-02-05 23:30:26 +0000

    settings.py change variable value

--- motordrive/settings.py
+++ motordrive/settings.py
@@ -25,7 +25,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@&h(!@zndewqu6&m^0$re(ghz(4!5n0_m*(wr!ct8ovp45+fh@'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
