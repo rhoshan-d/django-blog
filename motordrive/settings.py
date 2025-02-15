d42ed70a0ed820589a8e03373134a790efaecfa9
Rhoshan <rhoshan02@gmail.com>
2025-02-15 22:01:25 +0000

    Debug set to false.

--- motordrive/settings.py
+++ motordrive/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com' , '127.0.0.1']
 
