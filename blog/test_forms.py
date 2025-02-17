ac2b0c20a5206b2fd32172259769bf8d5e8e41c2
Rhoshan <rhoshan02@gmail.com>
2025-02-17 02:47:45 +0000

    added tests for Comment & VehicleProjectForm in test_views.py and test_forms.py

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,8 +1,9 @@
+import os
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
-from .forms import CommentForm
+from .forms import CommentForm, VehicleProjectForm
 
 class TestCommentForm(TestCase):
-
 
     def test_form_is_valid(self):
         comment_form = CommentForm({'body': 'This is a great post'})
@@ -11,3 +12,57 @@ class TestCommentForm(TestCase):
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
         self.assertFalse(comment_form.is_valid(), msg="Form is valid")
+
+class TestVehicleProjectForm(TestCase):
+
+    def test_form_is_valid(self):
+        image_path = './static/images/default.jpg'
+        with open(image_path, 'rb') as image:
+            form_data = {
+                'title': 'Project Title',
+                'slug': 'project-title',
+                'make': 'Car Make',
+                'model': 'Car Model',
+                'year': 2020,
+                'description': 'Project Description',
+            }
+            file_data = {
+                'vehicle_image': SimpleUploadedFile(name='default.jpg', content=image.read(), content_type='image/jpeg')
+            }
+            form = VehicleProjectForm(data=form_data, files=file_data)
+            self.assertTrue(form.is_valid(), msg="Form is invalid")
+
+    def test_form_is_invalid_missing_fields(self):
+        form_data = {
+            'title': '',
+            'make': 'Car Make',
+            'model': 'Car Model',
+            'year': 2020,
+            'description': 'Project Description',
+            'vehicle_image': 'path/to/image.jpg'
+        }
+        form = VehicleProjectForm(data=form_data)
+        self.assertFalse(form.is_valid(), msg="Form is valid")
+
+    def test_form_is_invalid_invalid_year(self):
+        form_data = {
+            'title': 'Project Title',
+            'make': 'Car Make',
+            'model': 'Car Model',
+            'year': 'invalid_year',
+            'description': 'Project Description',
+            'vehicle_image': 'path/to/image.jpg'
+        }
+        form = VehicleProjectForm(data=form_data)
+        self.assertFalse(form.is_valid(), msg="Form is valid")
+
+    def test_form_is_invalid_missing_image(self):
+        form_data = {
+            'title': 'Project Title',
+            'make': 'Car Make',
+            'model': 'Car Model',
+            'year': 2020,
+            'description': 'Project Description',
+        }
+        form = VehicleProjectForm(data=form_data)
+        self.assertFalse(form.is_valid(), msg="Form is valid")
--- blog/test_views.py
+++ blog/test_views.py
@@ -2,10 +2,9 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
 from .forms import CommentForm
-from .models import Post
+from .models import Post, Comment
 
 class TestBlogViews(TestCase):
-
 
     def setUp(self):
         """Create a superuser and a blog post"""
@@ -27,6 +26,40 @@ class TestBlogViews(TestCase):
 
     def test_successful_comment_submission(self):
         """Test for posting a comment on a post"""
+        self.client.login(username="myUsername", password="myPassword")
+        post_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(
+            reverse('post_detail', args=['blog-title']), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Comment submitted and awaiting approval',
+                      response.content)
+
+    def test_comment_edit(self):
+        """Test for editing a comment on a post"""
+        self.client.login(username="myUsername", password="myPassword")
+        comment = Comment.objects.create(
+            post=self.post, author=self.user, body="Original comment", approved=True)
+        post_data = {
+            'body': 'Updated comment.'
+        }
+        response = self.client.post(
+            reverse('comment_edit', args=['blog-title', comment.id]), post_data)
+        self.assertEqual(response.status_code, 302)
+        comment.refresh_from_db()
+        self.assertEqual(comment.body, 'Updated comment.')
+        self.assertFalse(comment.approved)
+
+    def test_comment_delete(self):
+        """Test for deleting a comment on a post"""
+        self.client.login(username="myUsername", password="myPassword")
+        comment = Comment.objects.create(
+            post=self.post, author=self.user, body="Comment to be deleted", approved=True)
+        response = self.client.post(
+            reverse('comment_delete', args=['blog-title', comment.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Comment.objects.filter(id=comment.id).exists())
         self.client.login(username="myUsername", password="myPassword")
         post_data = {
             'body': 'This is a test comment.'
